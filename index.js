7b0294bfc60cf16b301319ab3a71a3c7ebc396ca
Sandeep GC <sandeep_gc@hotmail.com>
2021-05-09 18:20:34 +1000

    added link to git hub and email

--- index.js
+++ index.js
@@ -13,27 +13,20 @@ const generateHTML = (answers) =>
 <body>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${answers.name}</h5>
     <h6 class="card-title">${answers.title}</h6>
   </div>
   <ul class="list-group list-group-flush">
     <li class="list-group-item">ID:${answers.id}</li>
-    <li class="list-group-item" href="#" class="card-link">Email:${answers.email}</li>
-    <li class="list-group-item"href="#" class="card-link">${answers.github}</li>
   </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
 </div>
-
-${answers.name}
-${answers.title}
-${answers.id}
-${answers.email}
-${answers.github}
+<div class="card-body">
+    <a href="#" class="card-link">Email:${answers.email}</a>
+  </div>
+  <div class="card-body">
+    <a href="#" class="card-link">Github Username:${answers.github}</a>
+  </div>
 
 </body>
 </html>`;
@@ -44,42 +37,42 @@ inquirer
       type: "input",
       name: "name",
       message: "What is the team member name",
-  },
-  {
+    },
+    {
       type: "input",
       name: "title",
       message: "What is the title? "
-  },
-  {
+    },
+    {
       type: "input",
       name: "id",
       message: "What is the id ",
-  },
-  {
+    },
+    {
       type: "list",
       name: "license",
       message: "Choose the appropriate license for this project: ",
       choices: [
-          "Apache",
-          "Academic",
-          "GNU",
-          "ISC",
-          "MIT",
-          "Mozilla",
-          "Open"
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
       ]
-  },
- 
-  {
+    },
+
+    {
       type: "input",
       name: "github",
       message: "Please enter your GitHub username: "
-  },
-  {
+    },
+    {
       type: "input",
       name: "email",
       message: "Please enter your email: "
-  }
+    }
   ])
   .then((answers) => {
     console.log(answers)
